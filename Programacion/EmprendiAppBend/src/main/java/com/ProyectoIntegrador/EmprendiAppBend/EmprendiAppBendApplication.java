1778cfa1448c0f95f5d84ef5f0c4d671d6903ccc
acj9189 <anecardenasja@unal.edu.co>
2019-10-18 00:20:08 -0500

    Actualizacion backend

---
+++ Programacion/EmprendiAppBend/src/main/java/com/ProyectoIntegrador/EmprendiAppBend/EmprendiAppBendApplication.java
@@ -0,0 +1,13 @@
+package com.ProyectoIntegrador.EmprendiAppBend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprendiAppBendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprendiAppBendApplication.class, args);
+	}
+
+}
---
+++ Programacion/EmprendiAppBend/src/test/java/com/ProyectoIntegrador/EmprendiAppBend/EmprendiAppBendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoIntegrador.EmprendiAppBend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprendiAppBendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
