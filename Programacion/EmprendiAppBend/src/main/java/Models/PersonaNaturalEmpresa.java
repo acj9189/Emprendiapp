8f981e5b227b9911f5bdcf03ac60e9e34da68493
acj9189 <anecardenasja@unal.edu.co>
2019-11-11 21:09:55 -0500

    Actualizcion Modelo PersonaNaturalEmpresa

--- Programacion/EmprendiAppBend/src/main/java/Models/PersonaNaturalEmpresa.java
+++ Programacion/EmprendiAppBend/src/main/java/Models/PersonaNaturalEmpresa.java
@@ -4,8 +4,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
-import org.springframework.beans.factory.annotation.Autowired;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import lombok.AllArgsConstructor;
