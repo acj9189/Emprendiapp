2de4e1deabf48e378a38a6e46e784a12b88e579a
acj9189 <acj8991@gmail.com>
2019-11-18 12:55:41 -0500

    Modificaciones para quitar Warnings

--- Programacion/EmprendiAppBend/src/main/java/Models/SocioInversor.java
+++ Programacion/EmprendiAppBend/src/main/java/Models/SocioInversor.java
@@ -24,7 +24,7 @@ public class SocioInversor extends Usuario{
 	
 	public SocioInversor() {
 		super();
-		// TODO Auto-generated constructor stub
+	
 	}
 	
 	public SocioInversor(String nombres, String apellidos, String telefono, String email, String descripcionIntereses,
@@ -40,85 +40,71 @@ public class SocioInversor extends Usuario{
 
 	@Override
 	public Integer getId() {
-		// TODO Auto-generated method stub
 		return super.getId();
 	}
 	
 	@Override
 	public void setId(Integer iD) {
-		// TODO Auto-generated method stub
 		super.setId(iD);
 	}
 	
 	@Override
 	public String getNombres() {
-		// TODO Auto-generated method stub
 		return super.getNombres();
 	}
 	
 	@Override
 	public void setNombres(String nombres) {
-		// TODO Auto-generated method stub
 		super.setNombres(nombres);
 	}
 	
 	@Override
 	public String getApellidos() {
-		// TODO Auto-generated method stub
 		return super.getApellidos();
 	}
 	
 	@Override
 	public void setApellidos(String apellidos) {
-		// TODO Auto-generated method stub
 		super.setApellidos(apellidos);
 	}
 	
 	@Override
 	public String getTelefono() {
-		// TODO Auto-generated method stub
 		return super.getTelefono();
 	}
 	
 	@Override
 	public void setTelefono(String telefono) {
-		// TODO Auto-generated method stub
 		super.setTelefono(telefono);
 	}
 	
 	@Override
 	public String getEmail() {
-		// TODO Auto-generated method stub
 		return super.getEmail();
 	}
 	
 	@Override
 	public void setEmail(String email) {
-		// TODO Auto-generated method stub
 		super.setEmail(email);
 	}
 	
 	@Override
 	public String getDescripcionIntereses() {
-		// TODO Auto-generated method stub
 		return super.getDescripcionIntereses();
 	}
 	
 	@Override
 	public void setDescripcionIntereses(String descripcionIntereses) {
-		// TODO Auto-generated method stub
 		super.setDescripcionIntereses(descripcionIntereses);
 	}
 	
 	@Override
 	public String getDireccionContacto() {
-		// TODO Auto-generated method stub
 		return super.getDireccionContacto();
 	}
 	
 	@Override
 	public void setDireccionContacto(String direccionContacto) {
-		// TODO Auto-generated method stub
 		super.setDireccionContacto(direccionContacto);
 	}
 	
