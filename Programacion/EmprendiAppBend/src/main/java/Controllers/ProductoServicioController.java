d1fd75d4969a9c8a10181c6b57bd33bc8e5e1c6a
acj9189 <acj8991@gmail.com>
2019-11-18 12:04:41 -0500

    Se creo el CRUD de ProductoServicio
    Creamos todos los metodos  que corresponden al CRUD de productoServicio pero falta ejecutarlo y hacerle pruebas...

---
+++ Programacion/EmprendiAppBend/src/main/java/Controllers/ProductoServicioController.java
@@ -0,0 +1,75 @@
+package Controllers;
+
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import Models.ProductoServicio;
+import Respositories.ProductoRepository;
+
+@RestController
+@RequestMapping("/")
+public class ProductoServicioController {
+
+	@Autowired
+	private ProductoRepository prodductoRepository;
+
+	@GetMapping("/productos")
+	private List<ProductoServicio> getAllProductos() {
+		return prodductoRepository.findAll();
+
+	}
+
+	@GetMapping("/producto/{id}")
+	private Optional<ProductoServicio> getProducto(@PathVariable Integer id) {
+		return prodductoRepository.findById(id);
+
+	}
+
+	@PostMapping("/producto")
+	private ProductoServicio NuevoProducto(@Valid @RequestBody ProductoServicio producto) {
+		return prodductoRepository.save(producto);
+
+	}
+
+	@PutMapping("/producto/{id}")
+	private ResponseEntity<ProductoServicio> updateProducto(@PathVariable(value = "id") Integer Id,
+			@Valid @RequestBody ProductoServicio productoDatails) throws ResourceNotFoundException {
+
+		ProductoServicio producto = prodductoRepository.findById(Id)
+				.orElseThrow(() -> new ResourceNotFoundException("No se encontro el producto o servicio  :: " + Id));
+
+		producto.setNombre(productoDatails.getNombre());
+		producto.setDescripcionProductoServicio(productoDatails.getDescripcionProblemaQSoluciona());
+		producto.setCostoUnidad(productoDatails.getCostoUnidad());
+		producto.setDescripcionProblemaQSoluciona(productoDatails.getDescripcionProblemaQSoluciona());
+
+		final ProductoServicio updatedProducto = prodductoRepository.save(producto);
+		return ResponseEntity.ok(updatedProducto);
+
+	}
+	
+	@DeleteMapping("/producto/{id}")
+	private boolean deleteProducto(@PathVariable(value = "id") Integer Id) throws ResourceNotFoundException {
+		boolean response = false;
+		
+		ProductoServicio deleteProduct = prodductoRepository.findById(Id).orElseThrow(() -> new ResourceNotFoundException("No se encontro el producto o servicio a eliminar :: " + Id));
+		prodductoRepository.delete(deleteProduct);
+		response = true;
+		return response;
+	}
+
+}
