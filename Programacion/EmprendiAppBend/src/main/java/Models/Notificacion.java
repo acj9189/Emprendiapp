bf226017c77b96caafd8e0adea6037fd5b23ab64
acj9189 <acj8991@gmail.com>
2019-11-18 12:48:53 -0500

    Continuacion con la implemetacion de la base de Datos
    Falta realizar todos los Constrains es decir todas las llaves foraneas...

---
+++ Programacion/EmprendiAppBend/src/main/java/Models/Notificacion.java
@@ -0,0 +1,71 @@
+package Models;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
+public class Notificacion {
+
+	@Id
+	@GeneratedValue
+	private int Id;
+	private String tipo;
+	private Date fechaNotificacion;
+	private String descripcion;
+
+	
+	public Notificacion() {
+
+	}
+
+	public Notificacion(String tipo, Date fechaNotificacion, String descripcion) {
+		this.tipo = tipo;
+		this.fechaNotificacion = fechaNotificacion;
+		this.descripcion = descripcion;
+	}
+
+	public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public Date getFechaNotificacion() {
+		return fechaNotificacion;
+	}
+
+	public void setFechaNotificacion(Date fechaNotificacion) {
+		this.fechaNotificacion = fechaNotificacion;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
