c8a808db647c6f009f874c11bb0f25853488f2fb
Cristian Buitrago <33074165+ccbuitragom@users.noreply.github.com>
2020-01-14 15:28:03 -0500

    Reorganizacion

--- Programacion/EmprendiAppFrontEnd/src/app/app-routing.module.ts
+++ Programacion/EmprendiAppFrontEnd/src/app/app-routing.module.ts
@@ -6,13 +6,13 @@ import { EditComponent } from './Usuario/edit/edit.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { BuscarFiltroComponent } from './buscar-filtro/buscar-filtro.component';
 import { EnviarMensajeComponent } from './enviar-mensaje/enviar-mensaje.component';
-import { InfoEmpresaComponent } from './info-empresa/info-empresa.component'
-import { InfoSocioComponent } from './info-socio/info-socio.component'
-import { InfoInversorComponent } from './info-inversor/info-inversor.component'
-import { InfoConsultorComponent } from './info-consultor/info-consultor.component'
-import { InfoAsesorComponent } from './info-asesor/info-asesor.component'
-import { RegistroEmpresaComponent } from './registro-empresa/registro-empresa.component'
-import { RegistroInversorComponent } from './registro-inversor/registro-inversor.component'
+import { InfoEmpresaComponent } from './Empresa/info-empresa/info-empresa.component'
+import { InfoSocioComponent } from './Socio/info-socio/info-socio.component'
+import { InfoInversorComponent } from './Inversor/info-inversor/info-inversor.component'
+import { InfoConsultorComponent } from './Consultor/info-consultor/info-consultor.component'
+import { InfoAsesorComponent } from './Asesor/info-asesor/info-asesor.component'
+import { RegistroEmpresaComponent } from './Empresa/registro-empresa/registro-empresa.component'
+import { RegistroInversorComponent } from './Inversor/registro-inversor/registro-inversor.component'
 
 const routes: Routes = [
   {path: 'listar', component: ListarComponent}, 
