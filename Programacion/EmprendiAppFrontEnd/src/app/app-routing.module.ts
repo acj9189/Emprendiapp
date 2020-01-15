4f95392ca8f5d98ce7702c4f0c21ec3faa4ead46
Cristian Buitrago <33074165+ccbuitragom@users.noreply.github.com>
2020-01-15 15:25:53 -0500

    arreglo del error
    se encontraba en las vistas recien creadas

--- Programacion/EmprendiAppFrontEnd/src/app/app-routing.module.ts
+++ Programacion/EmprendiAppFrontEnd/src/app/app-routing.module.ts
@@ -6,15 +6,33 @@ import { EditComponent } from './Usuario/edit/edit.component';
 import { PrincipalComponent } from './principal/principal.component';
 import { BuscarFiltroComponent } from './buscar-filtro/buscar-filtro.component';
 import { EnviarMensajeComponent } from './enviar-mensaje/enviar-mensaje.component';
-import { InfoEmpresaComponent } from './Empresa/info-empresa/info-empresa.component'
-import { InfoSocioComponent } from './Socio/info-socio/info-socio.component'
-import { InfoInversorComponent } from './Inversor/info-inversor/info-inversor.component'
-import { InfoConsultorComponent } from './Consultor/info-consultor/info-consultor.component'
-import { InfoAsesorComponent } from './Asesor/info-asesor/info-asesor.component'
-import { RegistroEmpresaComponent } from './Empresa/registro-empresa/registro-empresa.component'
-import { RegistroInversorComponent } from './Inversor/registro-inversor/registro-inversor.component'
+import { InfoEmpresaComponent } from './Empresa/info-empresa/info-empresa.component';
+import { InfoSocioComponent } from './Socio/info-socio/info-socio.component';
+import { InfoInversorComponent } from './Inversor/info-inversor/info-inversor.component';
+import { InfoConsultorComponent } from './Consultor/info-consultor/info-consultor.component';
+import { InfoAsesorComponent } from './Asesor/info-asesor/info-asesor.component';
+import { RegistroEmpresaComponent } from './Empresa/registro-empresa/registro-empresa.component';
+import { RegistroInversorComponent } from './Inversor/registro-inversor/registro-inversor.component';
+import { EditarEmpresaComponent } from './Empresa/editar-empresa/editar-empresa.component';
+import { RemoverEmpresaComponent } from './Empresa/remover-empresa/remover-empresa.component';
+import { RegistroAesorComponent } from './Asesor/registro-aesor/registro-aesor.component';
+import { RegitroConsultorComponent } from './Consultor/regitro-consultor/regitro-consultor.component';
+import { EditarProductoComponent } from './Producto/editar-producto/editar-producto.component';
+import { InfoProductoComponent } from './Producto/info-producto/info-producto.component';
+import { RegistrarProductoComponent } from './Producto/registrar-producto/registrar-producto.component';
+import { RemoverProductoComponent } from './Producto/remover-producto/remover-producto.component';
+import { RegistrarSocioComponent } from './Socio/registrar-socio/registrar-socio.component';
 
 const routes: Routes = [
+  {path: 'registrarSocio',component:RegistrarSocioComponent},
+  {path: 'removerProducto',component:RemoverProductoComponent},
+  {path: 'registroProducto',component:RegistrarProductoComponent},
+  {path: 'infoProducto',component:InfoProductoComponent},
+  {path: 'editarProducto',component:EditarProductoComponent},
+  {path: 'registroConsultor',component:RegitroConsultorComponent},
+  {path: 'registroAsesor',component:RegistroAesorComponent},
+  {path: 'removerEmpresa', component:RemoverEmpresaComponent},
+  {path: 'editEmpresa', component:EditarEmpresaComponent},
   {path: 'listar', component: ListarComponent}, 
   {path: 'add', component: AddComponent}, 
   {path: 'edit', component: EditComponent},
