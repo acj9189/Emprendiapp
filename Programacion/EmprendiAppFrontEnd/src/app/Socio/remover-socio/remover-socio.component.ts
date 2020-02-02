867af44b3da6a2edcf165960c6fb474d54c48e5d
Cristian Buitrago <33074165+ccbuitragom@users.noreply.github.com>
2020-02-02 12:18:21 -0500

    funcionando mas vistas

--- Programacion/EmprendiAppFrontEnd/src/app/Socio/remover-socio/remover-socio.component.ts
+++ Programacion/EmprendiAppFrontEnd/src/app/Socio/remover-socio/remover-socio.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SocioInversorServiceService } from "src/app/Service/socio-inversor-service.service";
+import { Router } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+import { SocioInversor } from 'src/app/Modelos/SocioInversor';
 
 @Component({
   selector: 'app-remover-socio',
@@ -7,9 +11,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RemoverSocioComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private service:SocioInversorServiceService, private router:Router,private rutaActiva: ActivatedRoute) { }
+  private id:number;
+  private socio:SocioInversor;
   ngOnInit() {
+    this.id=this.rutaActiva.snapshot.params.id;
+    this.service.infoSocio(this.id).subscribe(data=>{
+      this.socio=data;
+    });
   }
 
+  public mostrar(){
+    console.log(this.socio);
+  }
+
+  public retorno(){
+    this.router.navigate(['listaSocio']);
+  }
+
+  public eliminar(){
+    this.service.removeSocio(this.id).subscribe(data=>{
+      this.socio=data;
+    });
+    //this.router.navigate(['listaSocio']);
+    this.retorno();
+  }
 }
