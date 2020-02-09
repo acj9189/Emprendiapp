0fef7d28eda5c9dd39f0b6849aca19943e5aa22d
acj9189 <anecardenasja@unal.edu.co>
2020-02-08 22:18:55 -0500

    Actualizaciones

--- Programacion/EmprendiAppBackEnd/src/main/java/com/EmprendiApp/Controllers/AsesorConsultorController.java
+++ Programacion/EmprendiAppBackEnd/src/main/java/com/EmprendiApp/Controllers/AsesorConsultorController.java
@@ -1,5 +1,6 @@
 package com.EmprendiApp.Controllers;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
 
@@ -20,6 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.EmprendiApp.Models.AsesorConsultor;
+import com.EmprendiApp.Models.PersonaNaturalEmpresa;
 import com.EmprendiApp.Respositories.AsesorRepository;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
@@ -30,25 +32,51 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class AsesorConsultorController {
 	
 	@Autowired
-	private AsesorRepository AsesorRepository;
+	private AsesorRepository asesorRepository;
 	
 
 	@GetMapping("/all")
 	@ResponseBody
-	private List<AsesorConsultor> getAllAsesoresCOnsultores(){
-		return AsesorRepository.findAll();
+	private List<AsesorConsultor> getAllAsesoresConsultores(){
+		return asesorRepository.findAll();
+	}
+	
+	@GetMapping("/asesor/asesor/all")
+	@ResponseBody
+	public List<AsesorConsultor> getAllAsesores(){
+		List<AsesorConsultor> asesores = new LinkedList<>();
+		List<AsesorConsultor> asesoresConsultores = asesorRepository.findAll();
+		for(AsesorConsultor persoNatural: asesoresConsultores ) {
+			if(persoNatural.isTipoAsesorConsultor()) {
+				asesores.add(persoNatural);
+			}
+		}
+		return asesores;	
+	}
+	
+	@GetMapping("/asesor/consultor/all")
+	@ResponseBody
+	public List<AsesorConsultor> getAllConsultores(){
+		List<AsesorConsultor> consultores = new LinkedList<>();
+		List<AsesorConsultor> asesoresConsultores = asesorRepository.findAll();
+		for(AsesorConsultor consultor: asesoresConsultores ) {
+			if(!consultor.isTipoAsesorConsultor()) {
+				consultores.add(consultor);
+			}
+		}
+		return consultores;	
 	}
 	
 	@GetMapping("/asesor/{id}")
 	@ResponseBody
 	private Optional<AsesorConsultor> getAsesor(@PathVariable Integer id) {
-		return AsesorRepository.findById(id);
+		return asesorRepository.findById(id);
 	}
 
 	@PostMapping("/asesor")
 	@ResponseBody
 	private AsesorConsultor nuevoAsesor(@Valid @RequestBody AsesorConsultor asesor) {
-		return AsesorRepository.save(asesor);
+		return asesorRepository.save(asesor);
 	}
 
 	@PutMapping("/asesor/{id}")
@@ -56,15 +84,15 @@ public class AsesorConsultorController {
 	private ResponseEntity<AsesorConsultor> updateAsesor(@PathVariable(value = "id") Integer Id,
 			@Valid @RequestBody AsesorConsultor asesor) throws ResourceNotFoundException {
 
-		AsesorConsultor usuarioBuscado = AsesorRepository.findById(Id)
+		AsesorConsultor usuarioBuscado = asesorRepository.findById(Id)
 				.orElseThrow(() -> new ResourceNotFoundException("No se encontro el Usuario  :: " + Id));
 		usuarioBuscado.setAreasExperticia(asesor.getAreasExperticia());
 		usuarioBuscado.setAreasInteres(asesor.getAreasInteres());
 		usuarioBuscado.setConociminetoQAporta(asesor.getConociminetoQAporta());
 		usuarioBuscado.setCostoHora(asesor.getCostoHora());
-		usuarioBuscado.setTipoAsesorCOnsultor(asesor.isTipoAsesorCOnsultor());
+		usuarioBuscado.setTipoAsesorConsultor(asesor.isTipoAsesorConsultor());
 		usuarioBuscado.setUsuario(asesor.getUsuario());
-		final AsesorConsultor updateAsesor = AsesorRepository.save(usuarioBuscado);
+		final AsesorConsultor updateAsesor = asesorRepository.save(usuarioBuscado);
 		return ResponseEntity.ok(updateAsesor);
 	}
 
@@ -72,9 +100,9 @@ public class AsesorConsultorController {
 	@ResponseBody
 	private boolean deleteAsesor(@PathVariable(value = "id") Integer Id) throws ResourceNotFoundException {
 		boolean response = false;
-		AsesorConsultor deleteAsesor = AsesorRepository.findById(Id).orElseThrow(
+		AsesorConsultor deleteAsesor = asesorRepository.findById(Id).orElseThrow(
 				() -> new ResourceNotFoundException("No se encontro el Asesor o el consultar a eliminar :: " + Id));
-		AsesorRepository.delete(deleteAsesor);
+		asesorRepository.delete(deleteAsesor);
 		response = true;
 		return response;
 		
